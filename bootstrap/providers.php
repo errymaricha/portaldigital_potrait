6c3e22490df022c0d98692efae24d2b17dff7163
Erry Maricha <50708937+errymaricha@users.noreply.github.com>
2025-12-29 13:07:08 +0700

    Cleaning Code

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\DashboardPanelProvider::class,
-    App\Providers\Filament\UserPanelProvider::class,
+
 ];
